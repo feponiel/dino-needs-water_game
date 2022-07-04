540be4199be0c24308ec68f75f352d6a3a3be126
Felipe Macci <felipemacci@gmail.com>
2022-07-03 23:44:50 -0300

    completed game script

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,65 +1,157 @@
 const startScreen = document.querySelector('.overlay.start-screen')
 const endScreen = document.querySelector('.overlay.end-screen')
 
+const levelCounter = document.querySelector('.level-counter span')
+const bigLevelCounter = document.querySelector('.level-counter .big')
+
 const frame1 = document.querySelector('#dino img:nth-of-type(1)')
 const frame2 = document.querySelector('#dino img:nth-of-type(2)')
+const frame3 = document.querySelector('#dino img:nth-of-type(3)')
 
-const cactus = {
-    sprite: document.querySelector('.scene__cactus')
-}
+const pressSound = document.getElementById('press-sound')
+const jumpSound = document.getElementById('jump-sound')
+const levelSound = document.getElementById('level-sound')
+const damageSound = document.getElementById('damage-sound')
+const winSound = document.getElementById('win-sound')
 
-const dino = {
-    sprite: document.getElementById('dino'),
+const start = () => {
+    const handleLevelUp = setInterval(() => {
+        level++
+        levelSound.play()
 
-    isWalking: false,
+        bigLevelCounter.classList.add('up')
+        setTimeout(() => {
+            bigLevelCounter.classList.remove('up')
+        }, 400)
+        
+        levelCounter.innerText = level
 
-    isJumping: false,
+        if(level == 2) {
+            cactus.sprite.style.animation = 'none'
+            cactus.sprite.style.animation = 'scene 2s linear infinite'
+        } else if(level == 3) {
+            cactus.sprite.style.animation = 'none'
+            cactus.sprite.style.animation = 'scene 1s linear infinite'
+        } else {
+            endGame('win')
+        }
+    }, 32000)
 
-    walkAnimation() {
-        let currentFrame = 1
+    const endGame = (endType) => {
+        isGameOver = true
+
+        dino.isJumping = true
+
+        clearInterval(handleLevelUp)
+
+        if(endType == 'defeat') {
+            dino.sprite.style.filter = 'hue-rotate(200deg) saturate(300%)'
+            dino.isWalking = false
+            cactus.sprite.style.animationPlayState = 'paused'
+            waterBottle.sprite.style.animationPlayState = 'paused'
     
-        const walkingInterval = setInterval(() => {
-            if(currentFrame == 1) {
-                frame2.style.display = 'none'
-                frame1.style.display = 'block'
-                currentFrame++
-            } else {
-                frame1.style.display = 'none'
-                frame2.style.display = 'block'
-                currentFrame--
-            }
+            endScreen.querySelector('h1').innerText = 'Game Over'
+            endScreen.style.display = 'block'
+    
+            setTimeout(() => {
+                dino.sprite.style.display = 'none'
+            }, 50)
+        } else {
+            cactus.sprite.style.display = 'none'
 
-            if(!this.isWalking) {
-                clearInterval(walkingInterval)
-            }
-        }, 200)
-    },
+            waterBottle.sprite.style.display = 'block'
+            waterBottle.sprite.style.animation = 'scene 2s 3s linear 1'
 
-    jump() {
-        if(!this.isJumping) {
-            this.isJumping = true
-            this.sprite.style.animation = 'jump .7s ease-in-out'
+            endScreen.querySelector('h1').innerText = 'Won!'
 
             setTimeout(() => {
-                this.isJumping = false
-                this.sprite.style.animation = ''
-            }, 700)
+                endScreen.style.display = 'block'
+
+                waterBottle.sprite.style.display = 'none'
+
+                dino.isWalking = false
+    
+                setTimeout(() => {
+                    frame1.style.display = 'none'
+                    frame3.style.display = 'block'
+                }, 200)
+            }, 4800)
         }
-    },
-
-    handleCollision() {
-        setInterval(() => {
-            let dinoBottom = parseInt(window.getComputedStyle(this.sprite).getPropertyValue('bottom'))
-            let cactusLeft = parseInt(window.getComputedStyle(cactus.sprite).getPropertyValue('left'))
-        
-            if(cactusLeft > 10 && cactusLeft < 90 && dinoBottom <= 70) {
-                endGame('defeat')
-            }
-        }, 50)
     }
-}
 
-const startGame = () => {
+    const cactus = {
+        sprite: document.querySelector('.scene__cactus')
+    }
+
+    const waterBottle = {
+        sprite: document.querySelector('.scene__water-bottle')
+    }
+    
+    const dino = {
+        sprite: document.getElementById('dino'),
+    
+        isWalking: false,
+    
+        isJumping: false,
+    
+        isCollide: false,
+    
+        walkAnimation() {
+            let currentFrame = 1
+        
+            const walkingInterval = setInterval(() => {
+                if(currentFrame == 1) {
+                    frame2.style.display = 'none'
+                    frame1.style.display = 'block'
+                    currentFrame++
+                } else {
+                    frame1.style.display = 'none'
+                    frame2.style.display = 'block'
+                    currentFrame--
+                }
+    
+                if(!this.isWalking) {
+                    clearInterval(walkingInterval)
+                }
+            }, 200)
+        },
+    
+        jump() {
+            if(!this.isJumping) {
+                this.isJumping = true
+                this.sprite.style.animation = 'jump .8s ease-in-out'
+                jumpSound.play()
+    
+                setTimeout(() => {
+                    if(!isGameOver) {
+                        this.isJumping = false
+                        this.sprite.style.animation = ''
+                    }
+                }, 800)
+            }
+        },
+    
+        handleCollision() {
+            const checkIsCollideInterval = setInterval(() => {
+                let dinoBottom = parseInt(window.getComputedStyle(this.sprite).getPropertyValue('bottom'))
+                let cactusLeft = parseInt(window.getComputedStyle(cactus.sprite).getPropertyValue('left'))
+                let waterBottleLeft = parseInt(window.getComputedStyle(waterBottle.sprite).getPropertyValue('left'))
+                
+                if(cactusLeft > 10 && cactusLeft < 90 && dinoBottom <= 70) {
+                    damageSound.play()
+                    clearInterval(checkIsCollideInterval)
+                    endGame('defeat')
+                }
+
+                if(waterBottleLeft > 10 && waterBottleLeft < 90 && dinoBottom <= 70) {
+                    winSound.play()
+                    clearInterval(checkIsCollideInterval)
+                    waterBottle.sprite.style.animationPlayState = 'paused'
+                }
+            }, 50)
+        }
+    }
+    
     const handleKeyDown = () => {
         addEventListener('keydown', (e) => {
             if(e.keyCode === 32 || e.keyCode === 38) {
@@ -68,38 +160,30 @@ const startGame = () => {
         })
     }
 
-    handleKeyDown()
+    const initializeSprites = setTimeout(() => {
+        dino.sprite.style.display = 'block'
+        cactus.sprite.style.animation = 'scene 4s linear infinite'
+    }, 50)
+    
+    let isGameOver = false
+    let level = 1
+    
+    levelCounter.innerText = level
 
+    frame3.style.display = 'none'
+    dino.sprite.style.filter = 'none'
     dino.handleCollision()
     dino.isWalking = true
     dino.isJumping = false
     dino.walkAnimation()
+    cactus.sprite.style.display = 'block'
+    cactus.sprite.style.animation = 'none'
+    waterBottle.sprite.style.animation = 'none'
 
-    cactus.sprite.style.left = '1600px'
-    cactus.sprite.style.animationPlayState = 'running'
-    
-    startScreen.style.visibility = 'hidden'
-}
+    pressSound.play()
 
-const endGame = (endType) => {
-    if(endType == 'defeat') {
-        dino.sprite.style.filter = 'hue-rotate(200deg) saturate(300%)'
-        dino.isWalking = false
-        dino.isJumping = true
+    startScreen.style.display = 'none'
+    endScreen.style.display = 'none'
 
-        cactus.sprite.style.animationPlayState = 'paused'
-
-        endScreen.querySelector('h1').innerText = 'Game Over'
-        endScreen.style.display = 'block'
-
-        setTimeout(() => {
-            dino.sprite.style.display = 'none'
-        }, 50)
-    } else {
-        alert('won!')
-    }
-}
-
-const restartGame = () => {
-    location.href = '/'
+    handleKeyDown()
 }
