36c1646e6fd51b72ae6fbed6e75d511fbb1d1a57
Felipe Macci <felipemacci@gmail.com>
2022-08-26 18:41:35 -0300

    commenting the code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -17,10 +17,12 @@ class Game {
         this.bigLevelCounter = levelCounter.querySelector('.big')
     }
 
+    // Changes the title of the end screen
     setScreenTitle(title) {
         this.screen.querySelector('.screen__end-screen .content h1').innerText = title
     }
 
+    // Starts the game
     start() {
         pressSound.play()
 
@@ -36,6 +38,7 @@ class Game {
         obstacle.sprite.classList.replace('scene__obstacle--water-bottle', 'scene__obstacle--cactus')
     }
 
+    // Ends the game
     end() {
         this.isGameOver = true
         dino.isWalking = false
@@ -45,6 +48,7 @@ class Game {
 
         const obstacleType = obstacle.sprite.classList[1]
 
+        // Changing the end of the game based on the obstacle type
         switch(obstacleType) {
             case 'scene__obstacle--cactus':
                 damageSound.play()
@@ -84,11 +88,13 @@ class Dino {
         this.isJumping = false
     }
 
+    // Make the dinosaur walk
     walk() {
         this.isWalking = true
         let currentFrame = 1
         
         const walkingInterval = setInterval(() => {
+            // If the frame is equal to 1 it changes to 2 and vice versa
             if(currentFrame == 1) {
                 this.sprite.classList.replace('frame1', 'frame2')
                 currentFrame++
@@ -97,13 +103,17 @@ class Dino {
                 currentFrame--
             }
 
+            // If the dinosaur is not walking, the animation stops playing
             if(!this.isWalking) {
                 clearInterval(walkingInterval)
             }
         }, 200)
     }
 
+    // Make the dinosaur jump
     jump() {
+        // If the game is not over and the dinosaur jump is not turned off, check if he is already
+        // jumping, if not, make him jump
         if(!game.isGameOver && dino.isJumping != null) {
             if(!this.isJumping) {
                 jumpSound.play()
@@ -124,11 +134,13 @@ class Obstacle {
         this.sprite = document.querySelector('.scene__obstacle')
     }
 
+    // Initialize obstacle movement animation
     move() {
         this.sprite.style.animation = 'move 4s linear infinite'
     }
 }
 
+// Initialize the primary functions for the game to begin
 const start = () => {
     game.start()
     dino.walk()
@@ -139,21 +151,26 @@ const start = () => {
     handleLevelUp()
 }
 
+// Handle jump events
 const handleJump = () => {
     addEventListener('keydown', e => {
+        // If you press "SPACE" or "ARROW UP" key, the dinosaur will jump
         if(e.keyCode === 32 || e.keyCode === 38) {
             dino.jump()
         }
     })
 
+    // If you are on the mobile device and click on the screen, the dinosaur will jump
     document.querySelector('body').addEventListener('touchstart', () => dino.jump())
 }
 
+// Handle with the dinosaur collision on obstacle
 const handleCollision = () => {
     const checkIsCollideInterval = setInterval(() => {
         let dinoBottom = parseInt(window.getComputedStyle(dino.sprite).getPropertyValue('bottom'))
         let obstacleLeft = parseInt(window.getComputedStyle(obstacle.sprite).getPropertyValue('left'))
         
+        // Delimiting hitboxes and making the game end if they collide
         if(obstacleLeft > 10 && obstacleLeft < 90 && dinoBottom <= 70) {
             clearInterval(checkIsCollideInterval)
             game.end()
@@ -161,6 +178,7 @@ const handleCollision = () => {
     }, 50)
 }
 
+// Handle with the level up
 const handleLevelUp = () => {
     const levelUp = setInterval(() => {
         levelSound.play()
@@ -171,6 +189,7 @@ const handleLevelUp = () => {
         
         game.miniLevelCounter.innerText = game.level
 
+        // Changing the game and the behavior of obstacles based on the level
         switch(game.level) {
             case 2:
                 obstacle.sprite.style.animationDuration = '2s'
@@ -188,7 +207,9 @@ const handleLevelUp = () => {
         }
     }, 32000)
 
+    // Function that checks if the game is over every 50ms
     const checkIsGameOver = setInterval(() => {
+        // If the game is over, stop leveling up
         if(game.isGameOver) {
             clearInterval(levelUp)
             clearInterval(checkIsGameOver)
@@ -196,6 +217,7 @@ const handleLevelUp = () => {
     }, 50)
 }
 
+// Instantiating the main objects of the game
 const game = new Game(gameScreen, levelCounter)
 const dino = new Dino()
 const obstacle = new Obstacle()
